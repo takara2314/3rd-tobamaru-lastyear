f38087b6c1e1818bc31797642fef88e92f5da7e8
takara2314 <takara2314_TK@outlook.jp>
2022-12-02 09:03:35 +0900

    Fix: descriptionがSNSに表示されない不具合を修正

--- components/main.tsx
+++ components/main.tsx
@@ -14,7 +14,10 @@ const Main = ({ children, title, description }: Props) => {
       <NextSeo
         title={title}
         description="1994年に鳥羽商船高等専門学校の練習船として竣工された3代目鳥羽丸は、2025年3月に引退予定です。これまで多くの学生の航海実習に使われた鳥羽丸にありがとうを。"
-        openGraph={{ title, description }}
+        openGraph={{
+          title: title,
+          description: '1994年に鳥羽商船高等専門学校の練習船として竣工された3代目鳥羽丸は、2025年3月に引退予定です。これまで多くの学生の航海実習に使われた鳥羽丸にありがとうを。'
+        }}
       />
 
       <main className="p-10 text-lg w-screen h-[calc(100vh-4rem)] text-center overflow-x-hidden overflow-y-auto absolute top-[4rem] left-0">
