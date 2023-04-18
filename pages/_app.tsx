7427a5fe56e9f3bebe336bf7c44192fbaf02f6b3
takara2314 <takara2314_TK@outlook.jp>
2023-04-18 09:00:06 +0900

    Add: Googleのサイト認証タグを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,7 @@ const MyApp = ({ Component, pageProps, router }: AppProps) => {
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="google-site-verification" content="RYiFLBdutEA6x8UCiLv5OrUgIXP5AasQUSXiv8hMRgE" />
       </Head>
 
       <DefaultSeo
