f53826eb711edb65a946ee7a8dec090fbafb84a6
takara2314 <takara2314_TK@outlook.jp>
2023-10-13 08:35:23 +0900

    Add: ハンバーガーメニュー

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,39 @@
 <script>
   import "./reset.css";
   import "./global.css";
+
+  let isShowModal = false;
+
+  function handleClickHamburger() {
+    isShowModal = !isShowModal;
+  }
+
+  function handleModalClickBackground() {
+    isShowModal = false;
+  }
 </script>
 
 <menu>
-  <h1>
-    <a href="/">
-      <img
-        src="/logo.svg"
-        alt="ロゴ"
-        class="logo"
-      />
-    </a>
-  </h1>
-  <ul>
+  <div>
+    <button
+      class="hamburger-btn"
+      on:click={handleClickHamburger}
+    >
+      <div />
+      <div />
+      <div />
+    </button>
+    <h1 class="title">
+      <a href="/">
+        <img
+          src="/logo.svg"
+          alt="ロゴ"
+          class="logo"
+        />
+      </a>
+    </h1>
+  </div>
+  <ul class="pc">
     <li>
       <a href="/history">
         <div>
@@ -63,6 +83,74 @@
 
 <slot />
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+  class="modal-background"
+  style={isShowModal ? "display:block" : "display:none"}
+  on:click={handleModalClickBackground}
+/>
+<ul
+  class="sp"
+  style={isShowModal ? "display:block" : "display:none"}
+>
+  <li class="title-area">
+    <h1 class="title">
+      <a href="/">
+        <img
+          src="/logo.svg"
+          alt="ロゴ"
+          class="logo"
+        />
+      </a>
+    </h1>
+  </li>
+  <li>
+    <a href="/history">
+      <div>
+        <img
+          src="/icons/ship.svg"
+          alt="鳥羽丸の歴史"
+        />
+      </div>
+      鳥羽丸の歴史
+    </a>
+  </li>
+  <li>
+    <a href="/explore">
+      <div>
+        <img
+          src="/icons/compass.svg"
+          alt="鳥羽丸の歴史"
+        />
+      </div>
+      鳥羽丸探検
+    </a>
+  </li>
+  <li>
+    <a href="/point">
+      <div>
+        <img
+          src="/icons/ribbon.svg"
+          alt="鳥羽丸の歴史"
+        />
+      </div>
+      ここがスゴイ！
+    </a>
+  </li>
+  <li>
+    <a href="/contact">
+      <div>
+        <img
+          src="/icons/letter.svg"
+          alt="お問い合わせ"
+        />
+      </div>
+      お問い合わせ
+    </a>
+  </li>
+</ul>
+
 <footer>
   <ul>
     <li>
@@ -89,72 +177,182 @@
 
 <style>
   menu {
-    width: calc(100% - 3rem);
+    width: calc(100% - 2rem);
     height: 5rem;
     background-color: white;
     box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.25);
     border-radius: 1rem;
-    padding-left: 2rem;
-    padding-right: 2rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
     display: flex;
     flex-direction: row;
     position: fixed;
-    top: 1.5rem;
-    left: 1.5rem;
-    right: 1.5rem;
+    top: 1rem;
+    left: 1rem;
+    right: 1rem;
     z-index: 10;
   }
 
-  menu h1 {
-    width: 30rem;
-  }
-
-  menu ul {
-    width: calc(100% - 15rem);
+  menu div {
+    width: 100%;
     display: flex;
     flex-direction: row;
-    justify-content: end;
+    gap: 2rem;
   }
 
-  menu ul li {
-    font-size: 0.8rem;
-    margin-left: 5rem;
-  }
-
-  menu ul li a {
+  button.hamburger-btn {
+    width: 2rem;
     height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0.25rem;
+  }
+
+  button.hamburger-btn div {
+    width: 100%;
+    height: 0.25rem;
+    background-color: var(--primary-color);
+    border-radius: 0.25rem;
+  }
+
+  h1.title img {
+    height: 100%;
+  }
+
+  ul.pc {
+    display: none;
+  }
+
+  .modal-background {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 15;
+  }
+
+  ul.sp {
+    width: 80%;
+    height: 100%;
+    padding: 2rem;
+    background-color: white;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 20;
+  }
+
+  ul.sp li {
+    width: 100%;
+    height: 5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  ul.sp li.title-area {
+    margin-bottom: 3rem;
+  }
+
+  ul.sp li a {
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  ul.sp li a div {
+    width: 2rem;
+    height: 2rem;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
   }
 
-  menu ul li a div {
-    width: 2.5rem;
-    height: 2.5rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+  ul.sp li a div img {
+    width: 2rem;
   }
 
-  menu ul li a div img {
-    width: 100%;
-    margin-bottom: -0.5rem;
-  }
+  @media (min-width: 1024px) {
+    menu {
+      width: calc(100% - 3rem);
+      height: 5rem;
+      background-color: white;
+      box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.25);
+      border-radius: 1rem;
+      padding-left: 2rem;
+      padding-right: 2rem;
+      display: flex;
+      flex-direction: row;
+      position: fixed;
+      top: 1.5rem;
+      left: 1.5rem;
+      right: 1.5rem;
+      z-index: 10;
+    }
 
-  .logo {
-    height: 100%;
-  }
+    button.hamburger-btn {
+      display: none;
+    }
 
-  footer {
-    width: 100%;
-    background-color: var(--primary-color);
-    color: rgb(255, 255, 255);
-    text-align: left;
-    padding: 3rem;
-  }
+    h1.title {
+      width: 30rem;
+    }
 
-  footer ul {
-    margin-bottom: 2rem;
+    ul.pc {
+      width: calc(100% - 15rem);
+      display: flex;
+      flex-direction: row;
+      justify-content: end;
+    }
+
+    ul.pc li {
+      font-size: 0.8rem;
+      margin-left: 5rem;
+    }
+
+    ul.pc li a {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+
+    ul.pc li a div {
+      width: 2.5rem;
+      height: 2.5rem;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+
+    ul.pc li a div img {
+      width: 100%;
+      margin-bottom: -0.5rem;
+    }
+
+    .logo {
+      height: 100%;
+    }
+
+    footer {
+      width: 100%;
+      background-color: var(--primary-color);
+      color: rgb(255, 255, 255);
+      text-align: left;
+      padding: 3rem;
+    }
+
+    footer ul {
+      margin-bottom: 2rem;
+    }
   }
 </style>
