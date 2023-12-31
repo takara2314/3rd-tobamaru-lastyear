432269d73ddc786a66470df51613312da95008e2
takara2314 <takara2314_TK@outlook.jp>
2023-12-31 11:25:03 +0900

    Add: 鳥羽丸の歴史のページ

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -15,7 +15,36 @@
       鳥羽丸の歴史
     </H2>
     <div>
-      Coming soon...
+      <p>
+        商船学校の歴史は、校内練習船を中心に展開してきたと言っても過言ではありません。
+      </p>
+      <p>
+        練習船は、在学中に海と船に習熟し、運航に必要な知識や技能を修得するために欠かせないものです。本校は百十余年の歴史を持ち、時代に応じた練習船が建造されています。
+      </p>
+    </div>
+
+    <div class="link-area">
+      <a
+        href="https://www.maritime.toba-cmt.ac.jp/tobamaru/ship_history"
+        class="link"
+      >
+        鳥羽丸の歴史（商船学科ホームページ）
+      </a>
     </div>
   </article>
 </main>
+
+<style>
+  article > div {
+    padding-left: 4rem;
+    padding-right: 4rem;
+  }
+
+  .link-area {
+    margin-top: 1rem;
+  }
+
+  .link {
+    color: var(--primary-color);
+  }
+</style>
